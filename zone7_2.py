dd3124d1262fc820530ba37f8c2641dce11fbf0c
daniel.brown1 <daniel.brown1@mail.huji.ac.il>
2021-04-25 00:37:06 +0300

    trying to do a not bitwise subtraction between two date columns

--- zone7_2.py
+++ zone7_2.py
@@ -21,6 +21,19 @@ training_sessions[DAYS_BEFORE], training_sessions[DAYS_AFTER] = '?', '?'
 match_days = pd.read_csv("match_days.csv")
 match_days[GAME_DAY_TITLE] = YES_GAME_DAY
 match_days[DAYS_BEFORE], match_days[DAYS_AFTER] = 0, 0
-data = pd.concat([match_days, training_sessions], ignore_index = True)
+
+data = pd.concat([match_days, training_sessions], ignore_index = True, sort=True)
 
 data = data.sort_values(["id", "Date"])
+match_id = match_days.sort_vaslues(["id", "Date"]).groupby("id")
+training_id = training_sessions.sort_values(["id", "Date"]).groupby("id")
+id_groups = data.groupby("id")
+for player in match_id.groups:
+    p_matches = match_id.get_group(player)
+    p_training = training_id.get_group(player)
+    player_dates = p_matches['Date'] - p_training['Date']
+
+    # player[DAYS_BEFORE] = argmin(match_id_groups[date] - training_id_groups[date]) s.t. > 0
+    # player[DAYS_AFTER] = abs (argmin(match_id_groups[date] - training_id_groups[date]) s.t. < 0)
+    print(player)
+print(id_groups)
