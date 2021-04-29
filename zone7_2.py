ea15826b0f87488a9212573976f6c1baad411965
daniel.brown1 <daniel.brown1@mail.huji.ac.il>
2021-04-29 16:32:17 +0300

    done with 'compute_days_before', starting to work on 'compute_days_after'

--- zone7_2.py
+++ zone7_2.py
@@ -11,50 +11,50 @@ YES_GAME_DAY = 1
 NOT_GAME_DAY = 0
 
 
-def compute_days_before(p_training_dates, p_matches_dates):
-    training_dates = list((pd.to_datetime(p_training_dates[DATE])).values)
+def compute_days_before(training_dates, p_matches_dates):
+    if type(p_matches_dates) is list:
+        return [-1] * len(p_training)
+    # training_dates = list((pd.to_datetime(p_training_dates[DATE])).values)
     matches_dates = pd.to_datetime(p_matches_dates[DATE])
     i = 0
+    days_before = []
     for match in matches_dates.values:
         training_dates = training_dates[i:]
         subt = (pd.to_datetime(match) - pd.to_datetime(training_dates)).days
-        answer = [val for val in subt if val > 0]
-        print(answer)
+        days_before += [val for val in subt if val > 0]
         i = pd.DataFrame(subt).lt(0).idxmax()[0]
-        # print("MATCH: ", match)
-        # print("TRAINING DATES:", training_dates.values)
-        # print("SUBTRACTION: ", subt)
+    days_before += [-1] * (len(training_dates) - len(days_before))
+    return days_before
+
+
+def compute_days_after(training_dates, p_matches_dates):
     return
 
 
-def compute_days_after():
-    return
-
-
-training_sessions = pd.read_csv("training_sessions.csv")
+training_sessions = pd.read_csv("training_sessions.csv").sort_values([ID, DATE])
 training_sessions[GAME_DAY_TITLE] = NOT_GAME_DAY
-training_sessions[DAYS_BEFORE], training_sessions[DAYS_AFTER] = '?', '?'
+# training_sessions[DAYS_BEFORE], training_sessions[DAYS_AFTER] = '?', '?'
 
-match_days = pd.read_csv("match_days.csv")
+match_days = pd.read_csv("match_days.csv").sort_values([ID, DATE])
 match_days[GAME_DAY_TITLE] = YES_GAME_DAY
 match_days[DAYS_BEFORE], match_days[DAYS_AFTER] = 0, 0
 
-data = pd.concat([match_days, training_sessions], ignore_index = True, sort = True)
-
-matches_by_id = match_days.sort_values([ID, DATE]).groupby(ID)
-training_by_id = training_sessions.sort_values([ID, DATE]).groupby(ID)
-id_groups = data.sort_values([ID, DATE]).groupby(ID)
+matches_by_id = match_days.groupby(ID)
+training_by_id = training_sessions.groupby(ID)
 # iterating over the players: each one is a group gotten from the pd.groupby() function
-for player in id_groups.groups:
-    print("PLAYER ID: ", player)
-    # fetching the player's matches
-    p_matches = matches_by_id.get_group(player)
-    # fetching the player's trainings
-    p_training = training_by_id.get_group(player)
+before = []
+after = []
+for player in training_by_id.groups:
+    p_training = list((pd.to_datetime(training_by_id.get_group(player)[DATE])).values)
+    try:
+        before += compute_days_before(p_training, matches_by_id.get_group(player))
+        after += compute_days_after(p_training, matches_by_id.get_group(player))
+    except KeyError:
+        before += compute_days_before(p_training, [])
 
-    # player_dates = pd.to_datetime(pd.to_datetime(p_training['Date']).values[0] - pd.to_datetime(p_matches['Date']).values).day
-    compute_days_before(p_training, p_matches)
+# adding -1 for non valid rows (e.g. trainings with no id value)
+before += [-1] * (len(training_sessions) - len(before))
+training_sessions[DAYS_BEFORE] = pd.Series(before).values
 
-    # Psuedo code:
-    # player[DAYS_BEFORE] = argmin(match_id_groups[date] - training_id_groups[date]) s.t. > 0
-    # player[DAYS_AFTER] = abs (argmin(match_id_groups[date] - training_id_groups[date]) s.t. < 0)
+data = pd.concat([match_days, training_sessions], ignore_index = True, sort = True).sort_values([ID, DATE])
+print(data)
